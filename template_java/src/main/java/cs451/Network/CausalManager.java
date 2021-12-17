a99458f5c42301c0358619fe1d24459737f48294
Abdeljalil Moussa <jalil.msa@gmail.com>
2021-12-17 19:44:02 +0100

    move awaitingMessage class

--- template_java/src/main/java/cs451/Network/CausalManager.java
+++ template_java/src/main/java/cs451/Network/CausalManager.java
@@ -138,4 +138,19 @@ public class CausalManager implements URBDelivery, Broadcast {
         r.start();
     }
 
+    private static final class AwaitingMessage {
+
+        public int msgId;
+        public int basisId;
+        public byte[] data;
+        public int[] vectorClock;
+
+        public AwaitingMessage(int basisId, int msgId, int[] vectorClock, byte[] data) {
+            this.data = data;
+            this.msgId = msgId;
+            this.basisId = basisId;
+            this.vectorClock = vectorClock;
+        }
+    }
+
 }
